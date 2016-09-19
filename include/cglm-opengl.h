5115814d5b53ed4c9da2803e6ab7c5898e9bef7c
Recep Aslantas <info@recp.me>
2016-09-19 19:03:07 +0300

    glm --> opengl helper function

---
+++ include/cglm-opengl.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef cglm_mat_opengl_h
+#define cglm_mat_opengl_h
+
+#include "cglm.h"
+
+CGLM_INLINE
+void
+glm_mat4_uniform(mat4 m, GLint location){
+  glUniformMatrix4fv(location, 1, GL_FALSE, (GLfloat *)m);
+}
+
+#endif /* cglm_mat_opengl_h */
