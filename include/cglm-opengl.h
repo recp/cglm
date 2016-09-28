6a0059814435280e1aa676175fdcf24c510d547f
Recep Aslantas <info@recp.me>
2016-09-28 18:04:09 +0300

    msvc: opengl header

--- include/cglm-opengl.h
+++ include/cglm-opengl.h
@@ -8,7 +8,25 @@
 #ifndef cglm_mat_opengl_h
 #define cglm_mat_opengl_h
 
+#ifdef __APPLE__
+#  include <OpenGL/gl3.h>
+#else
+#  ifdef _WIN32
+#    include <windows.h>
+#  endif
+#  include <GL/gl.h>
+#endif
+
 #include "cglm.h"
+
+#ifdef _WIN32
+extern
+void
+glUniformMatrix4fv(GLint location,
+                   GLsizei count,
+                   GLboolean transpose,
+                   const GLfloat *value);
+#endif
 
 CGLM_INLINE
 void
