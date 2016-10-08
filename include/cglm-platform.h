c428b79f98cab30aa22c06b37245e837f57870bc
Recep Aslantas <info@recp.me>
2016-10-08 16:37:20 +0300

    move platform specific codes to platform header

---
+++ include/cglm-platform.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef cglm_platform_h
+#define cglm_platform_h
+
+#ifdef __APPLE__
+#  include <OpenGL/gl3.h>
+#else
+#  ifdef _WIN32
+#    include <windows.h>
+#  endif
+#  include <GL/gl.h>
+#endif
+
+#ifdef _WIN32
+extern
+void
+glUniformMatrix4fv(GLint location,
+                   GLsizei count,
+                   GLboolean transpose,
+                   const GLfloat *value);
+#endif
+
+#endif /* cglm_platform_h */
