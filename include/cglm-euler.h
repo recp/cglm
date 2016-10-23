0ff0579456eff6d239d1e3ef1ec5329033e056c6
Recep Aslantas <info@recp.me>
2016-10-23 23:27:57 +0300

    fix euler matrix

--- include/cglm-euler.h
+++ include/cglm-euler.h
@@ -38,6 +38,9 @@ glm_euler(float yaw,
   dest[0][3] = 0.0f;
   dest[1][3] = 0.0f;
   dest[2][3] = 0.0f;
+  dest[3][0] = 0.0f;
+  dest[3][1] = 0.0f;
+  dest[3][2] = 0.0f;
   dest[3][3] = 1.0f;
 }
 
@@ -69,6 +72,9 @@ glm_euler_zxy(float yaw,
   dest[0][3] = 0.0f;
   dest[1][3] = 0.0f;
   dest[2][3] = 0.0f;
+  dest[3][0] = 0.0f;
+  dest[3][1] = 0.0f;
+  dest[3][2] = 0.0f;
   dest[3][3] = 1.0f;
 }
 
