072ccf5d63984a55d0adeb49642748c528042938
Recep Aslantas <info@recp.me>
2016-12-10 11:30:58 +0200

    vec: fix assignment op

--- include/cglm-vec.h
+++ include/cglm-vec.h
@@ -357,7 +357,7 @@ glm_vec4_normalize_to(vec4 vec, vec4 dest) {
   norm = glm_vec4_norm(vec);
 
   if (norm == 0.0f) {
-    dest[0] = dest[1] = dest[2] = dest[3] == 0.0f;
+    dest[0] = dest[1] = dest[2] = dest[3] = 0.0f;
     return;
   }
 
