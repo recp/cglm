b00f2b9ccc8a96b8c1b1a84bd93d7e014bc24343
Carsten Hartenfels <carsten.hartenfels@gmail.com>
2018-09-19 20:46:37 +0200

    Replace M_PI_4 in test_affine with CGLM_PI_4
    As in the previous commit, because it's non-standard and depending on
    your settings it can fail to compile because of it.

--- test/src/test_affine.c
+++ test/src/test_affine.c
@@ -12,7 +12,7 @@ test_affine(void **state) {
   mat4 t1, t2, t3, t4, t5;
 
   /* test translate is postmultiplied */
-  glmc_rotate_make(t1, M_PI_4, GLM_YUP);
+  glmc_rotate_make(t1, CGLM_PI_4, GLM_YUP);
   glm_translate_make(t2, (vec3){34, 57, 36});
 
   glmc_mat4_mul(t1, t2, t3); /* R * T */
@@ -21,16 +21,16 @@ test_affine(void **state) {
   test_assert_mat4_eq(t1, t3);
 
   /* test rotate is postmultiplied */
-  glmc_rotate_make(t1, M_PI_4, GLM_YUP);
+  glmc_rotate_make(t1, CGLM_PI_4, GLM_YUP);
   glm_translate_make(t2, (vec3){34, 57, 36});
 
   glmc_mat4_mul(t2, t1, t3); /* T * R */
 
-  glm_rotate(t2, M_PI_4, GLM_YUP);
+  glm_rotate(t2, CGLM_PI_4, GLM_YUP);
   test_assert_mat4_eq(t2, t3);
 
   /* test scale is postmultiplied */
-  glmc_rotate_make(t1, M_PI_4, GLM_YUP);
+  glmc_rotate_make(t1, CGLM_PI_4, GLM_YUP);
   glm_translate_make(t2, (vec3){34, 57, 36});
   glm_scale_make(t4, (vec3){3, 5, 6});
 
@@ -41,7 +41,7 @@ test_affine(void **state) {
   test_assert_mat4_eq(t3, t5);
 
   /* test translate_x */
-  glmc_rotate_make(t1, M_PI_4, GLM_YUP);
+  glmc_rotate_make(t1, CGLM_PI_4, GLM_YUP);
   glm_translate_make(t2, (vec3){34, 0, 0});
 
   glmc_mat4_mul(t1, t2, t3); /* R * T */
@@ -49,7 +49,7 @@ test_affine(void **state) {
   test_assert_mat4_eq(t1, t3);
 
   /* test translate_y */
-  glmc_rotate_make(t1, M_PI_4, GLM_YUP);
+  glmc_rotate_make(t1, CGLM_PI_4, GLM_YUP);
   glm_translate_make(t2, (vec3){0, 57, 0});
 
   glmc_mat4_mul(t1, t2, t3); /* R * T */
@@ -57,7 +57,7 @@ test_affine(void **state) {
   test_assert_mat4_eq(t1, t3);
 
   /* test translate_z */
-  glmc_rotate_make(t1, M_PI_4, GLM_YUP);
+  glmc_rotate_make(t1, CGLM_PI_4, GLM_YUP);
   glm_translate_make(t2, (vec3){0, 0, 36});
 
   glmc_mat4_mul(t1, t2, t3); /* R * T */
@@ -65,43 +65,43 @@ test_affine(void **state) {
   test_assert_mat4_eq(t1, t3);
 
   /* test rotate_x */
-  glmc_rotate_make(t1, M_PI_4, (vec3){1, 0, 0});
+  glmc_rotate_make(t1, CGLM_PI_4, (vec3){1, 0, 0});
   glm_translate_make(t2, (vec3){34, 57, 36});
 
   glmc_mat4_mul(t2, t1, t3); /* T * R */
 
-  glm_rotate_x(t2, M_PI_4, t2);
+  glm_rotate_x(t2, CGLM_PI_4, t2);
   test_assert_mat4_eq(t2, t3);
 
   /* test rotate_y */
-  glmc_rotate_make(t1, M_PI_4, (vec3){0, 1, 0});
+  glmc_rotate_make(t1, CGLM_PI_4, (vec3){0, 1, 0});
   glm_translate_make(t2, (vec3){34, 57, 36});
 
   glmc_mat4_mul(t2, t1, t3); /* T * R */
 
-  glm_rotate_y(t2, M_PI_4, t2);
+  glm_rotate_y(t2, CGLM_PI_4, t2);
   test_assert_mat4_eq(t2, t3);
 
   /* test rotate_z */
-  glmc_rotate_make(t1, M_PI_4, (vec3){0, 0, 1});
+  glmc_rotate_make(t1, CGLM_PI_4, (vec3){0, 0, 1});
   glm_translate_make(t2, (vec3){34, 57, 36});
 
   glmc_mat4_mul(t2, t1, t3); /* T * R */
 
-  glm_rotate_z(t2, M_PI_4, t2);
+  glm_rotate_z(t2, CGLM_PI_4, t2);
   test_assert_mat4_eq(t2, t3);
 
   /* test rotate */
-  glmc_rotate_make(t1, M_PI_4, (vec3){0, 0, 1});
+  glmc_rotate_make(t1, CGLM_PI_4, (vec3){0, 0, 1});
   glm_translate_make(t2, (vec3){34, 57, 36});
 
   glmc_mat4_mul(t2, t1, t3); /* T * R */
-  glmc_rotate(t2, M_PI_4, (vec3){0, 0, 1});
+  glmc_rotate(t2, CGLM_PI_4, (vec3){0, 0, 1});
 
   test_assert_mat4_eq(t3, t2);
 
   /* test scale_uni */
-  glmc_rotate_make(t1, M_PI_4, GLM_YUP);
+  glmc_rotate_make(t1, CGLM_PI_4, GLM_YUP);
   glm_translate_make(t2, (vec3){34, 57, 36});
   glm_scale_make(t4, (vec3){3, 3, 3});
 
