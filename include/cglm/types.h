7405d5e1d0be0305b21b0a31515f2d72fd6aca97
Recep Aslantas <info@recp.me>
2018-09-21 23:33:42 +0300

    Update types.h

--- include/cglm/types.h
+++ include/cglm/types.h
@@ -45,7 +45,6 @@ typedef                   vec3  mat3[3];
 typedef CGLM_ALIGN_IF(16) vec4  mat4[4];
 #endif
 
-
 typedef vec4                    versor;
 
 #define GLM_E         2.71828182845904523536028747135266250   /* e              */
@@ -62,23 +61,23 @@ typedef vec4                    versor;
 #define GLM_SQRT2     1.41421356237309504880168872420969808   /* sqrt(2)        */
 #define GLM_SQRT1_2   0.707106781186547524400844362104849039  /* 1/sqrt(2)      */
 
-#define GLM_Ef        ((float) GLM_E)
-#define GLM_LOG2Ef    ((float) GLM_LOG2E)
-#define GLM_LOG10Ef   ((float) GLM_LOG10E)
-#define GLM_LN2f      ((float) GLM_LN2)
-#define GLM_LN10f     ((float) GLM_LN10)
-#define GLM_PIf       ((float) GLM_PI)
-#define GLM_PI_2f     ((float) GLM_PI_2)
-#define GLM_PI_4f     ((float) GLM_PI_4)
-#define GLM_1_PIf     ((float) GLM_1_PI)
-#define GLM_2_PIf     ((float) GLM_2_PI)
-#define GLM_2_SQRTPIf ((float) GLM_2_SQRTPI)
-#define GLM_SQRT2f    ((float) GLM_SQRT2)
-#define GLM_SQRT1_2f  ((float) GLM_SQRT1_2)
+#define GLM_Ef        ((float)GLM_E)
+#define GLM_LOG2Ef    ((float)GLM_LOG2E)
+#define GLM_LOG10Ef   ((float)GLM_LOG10E)
+#define GLM_LN2f      ((float)GLM_LN2)
+#define GLM_LN10f     ((float)GLM_LN10)
+#define GLM_PIf       ((float)GLM_PI)
+#define GLM_PI_2f     ((float)GLM_PI_2)
+#define GLM_PI_4f     ((float)GLM_PI_4)
+#define GLM_1_PIf     ((float)GLM_1_PI)
+#define GLM_2_PIf     ((float)GLM_2_PI)
+#define GLM_2_SQRTPIf ((float)GLM_2_SQRTPI)
+#define GLM_SQRT2f    ((float)GLM_SQRT2)
+#define GLM_SQRT1_2f  ((float)GLM_SQRT1_2)
 
 /* DEPRECATED! use GLM_PI and friends */
-#define CGLM_PI    GLM_PIf
-#define CGLM_PI_2  GLM_PI_2f
-#define CGLM_PI_4  GLM_PI_4f
+#define CGLM_PI       GLM_PIf
+#define CGLM_PI_2     GLM_PI_2f
+#define CGLM_PI_4     GLM_PI_4f
 
 #endif /* cglm_types_h */
