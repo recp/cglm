406d09e08555501a60902608060d2fa61e075f21
Recep Aslantas <info@recp.me>
2020-01-19 21:48:55 +0300

    make plane zero if norm is zero after normalization

--- include/cglm/plane.h
+++ include/cglm/plane.h
@@ -30,7 +30,14 @@
 CGLM_INLINE
 void
 glm_plane_normalize(vec4 plane) {
-  glm_vec4_scale(plane, 1.0f / glm_vec3_norm(plane), plane);
+  float norm;
+  
+  if ((norm = glm_vec3_norm(plane)) == 0.0f) {
+    glm_vec4_zero(plane);
+    return;
+  }
+  
+  glm_vec4_scale(plane, 1.0f / norm, plane);
 }
 
 #endif /* cglm_plane_h */
