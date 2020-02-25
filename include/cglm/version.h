010b88ee7acd164de59a2e43a0d3fc1081c6fcdb
Recep Aslantas <info@recp.me>
2020-02-25 22:13:25 +0300

    set version to v0.7.0 for vec2/mat2 release

--- include/cglm/version.h
+++ include/cglm/version.h
@@ -9,7 +9,7 @@
 #define cglm_version_h
 
 #define CGLM_VERSION_MAJOR 0
-#define CGLM_VERSION_MINOR 6
-#define CGLM_VERSION_PATCH 3
+#define CGLM_VERSION_MINOR 7
+#define CGLM_VERSION_PATCH 0
 
 #endif /* cglm_version_h */
