339adab783b8470f137f03b4ea0465986374e7a9
Uwila <uwila@runbox.com>
2020-04-03 17:56:53 +0200

    Align variables in ray code

--- include/cglm/call/ray.h
+++ include/cglm/call/ray.h
@@ -14,7 +14,12 @@ extern "C" {
 
 CGLM_EXPORT
 bool
-glmc_ray_triangle(vec3 origin, vec3 direction, vec3 v0, vec3 v1, vec3 v2, float *d);
+glmc_ray_triangle(vec3   origin,
+                  vec3   direction,
+                  vec3   v0,
+                  vec3   v1,
+                  vec3   v2,
+                  float *d);
     
 #ifdef __cplusplus
 }
--- include/cglm/ray.h
+++ include/cglm/ray.h
@@ -7,7 +7,12 @@
 
 /*
  Functions:
-   CGLM_INLINE bool glm_line_triangle_intersect(vec3 origin, vec3 direction, vec3 v0, vec3 v1, vec3 v2, float *d);
+   CGLM_INLINE bool glm_line_triangle_intersect(vec3   origin,
+                                                vec3   direction,
+                                                vec3   v0,
+                                                vec3   v1,
+                                                vec3   v2,
+                                                float *d);
 */
 
 #ifndef cglm_ray_h
@@ -29,7 +34,12 @@
 
 CGLM_INLINE
 bool
-glm_ray_triangle(vec3 origin, vec3 direction, vec3 v0, vec3 v1, vec3 v2, float *d) {
+glm_ray_triangle(vec3   origin,
+                 vec3   direction,
+                 vec3   v0,
+                 vec3   v1,
+                 vec3   v2,
+                 float *d) {
   vec3        edge1, edge2, p, t, q;
   float       det, inv_det, u, v, dist;
   const float epsilon = 0.000001;
