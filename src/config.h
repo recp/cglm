099522ada3588c3ccb7bad0b4524602c6993541a
Recep Aslantas <info@recp.me>
2020-05-02 22:32:57 +0300

    fix LICENSE alignments

--- src/config.h
+++ src/config.h
@@ -1,9 +1,9 @@
 /*
-* Copyright (c), Recep Aslantas.
-*
-* MIT License (MIT), http://opensource.org/licenses/MIT
-* Full license can be found in the LICENSE file
-*/
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
 
 #ifndef cglm__config__h_
 #define cglm__config__h_
--- test/src/test_struct.c
+++ test/src/test_struct.c
@@ -1,9 +1,9 @@
 /*
-* Copyright (c), Recep Aslantas.
-*
-* MIT License (MIT), http://opensource.org/licenses/MIT
-* Full license can be found in the LICENSE file
-*/
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
 
 #include "test_common.h"
 
