269bdb3dbd07001b4345d8b9c88f235b27d4804a
Recep Aslantas <info@recp.me>
2020-05-11 20:19:05 +0300

    fix arm neon build

--- include/cglm/vec4.h
+++ include/cglm/vec4.h
@@ -680,7 +680,7 @@ glm_vec4_negate_to(vec4 v, vec4 dest) {
 #if defined( __SSE__ ) || defined( __SSE2__ )
   glmm_store(dest, _mm_xor_ps(glmm_load(v), _mm_set1_ps(-0.0f)));
 #elif defined(CGLM_NEON_FP)
-  vst1q_f32(dest, veorq_s32(vld1q_f32(v), vdupq_n_f32(-0.0f)));
+  vst1q_f32(dest, vnegq_f32(vld1q_f32(v)));
 #else
   dest[0] = -v[0];
   dest[1] = -v[1];
