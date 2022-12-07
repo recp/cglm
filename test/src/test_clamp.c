ac9461778c8dbf5de3e04cd12b39f55228fa382c
Recep Aslantas <info@recp.me>
2022-12-07 12:57:26 +0300

    possible fix for https://github.com/recp/cglm/issues/266

--- test/src/test_clamp.c
+++ test/src/test_clamp.c
@@ -11,9 +11,9 @@ TEST_IMPL(clamp) {
   vec3 v3 = {15.07f, 0.4f, 17.3f};
   vec4 v4 = {5.07f,  2.3f, 1.3f, 1.4f};
 
-  ASSERT(glm_clamp(1.6f, 0.0f, 1.0f)  == 1.0f)
-  ASSERT(glm_clamp(-1.6f, 0.0f, 1.0f) == 0.0f)
-  ASSERT(glm_clamp(0.6f, 0.0f, 1.0f)  == 0.6f)
+  ASSERT(glm_eq(glm_clamp(1.6f, 0.0f, 1.0f),  1.0f))
+  ASSERT(glm_eq(glm_clamp(-1.6f, 0.0f, 1.0f), 0.0f))
+  ASSERT(glm_eq(glm_clamp(0.6f, 0.0f, 1.0f),  0.6f))
 
   glm_vec3_clamp(v3, 0.0, 1.0);
   glm_vec4_clamp(v4, 1.5, 3.0);
