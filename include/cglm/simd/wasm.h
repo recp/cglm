7ca9a64ecf9cdc0f9871272a7a18dd8f2f073df1
myfreeer <myfreeer@users.noreply.github.com>
2023-04-01 19:38:57 +0800

    simd128: code style

--- include/cglm/simd/wasm.h
+++ include/cglm/simd/wasm.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
 
 #ifndef cglm_simd_wasm_h
 #define cglm_simd_wasm_h
