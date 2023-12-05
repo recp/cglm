22140d8e1bef1bbb3c6f765d7f7eb820d2d2c349
BeeverFeever <scottyadam6@gmail.com>
2023-12-05 19:48:54 +1100

    Fix test

--- test/src/test_vec4.h
+++ test/src/test_vec4.h
@@ -562,10 +562,10 @@ TEST_IMPL(GLM_PREFIX, vec4_subsub) {
   
   GLM(vec4_subsub)(v1, v2, v4);
 
-  ASSERT(test_eq(v3[0] - v1[0] - v2[0], v4[0]))
-  ASSERT(test_eq(v3[1] - v1[1] - v2[1], v4[1]))
-  ASSERT(test_eq(v3[2] - v1[2] - v2[2], v4[2]))
-  ASSERT(test_eq(v3[3] - v1[3] - v2[3], v4[3]))
+  ASSERT(test_eq(v3[0] - (v1[0] - v2[0]), v4[0]))
+  ASSERT(test_eq(v3[1] - (v1[1] - v2[1]), v4[1]))
+  ASSERT(test_eq(v3[2] - (v1[2] - v2[2]), v4[2]))
+  ASSERT(test_eq(v3[3] - (v1[3] - v2[3]), v4[3]))
   
   TEST_SUCCESS
 }
