ae1bee74810ceb81723bad6e997cebc49e6a4896
Marcin <marcin.konow@lczyk.xyz>
2025-01-17 14:46:07 +0000

    doc

--- include/cglm/noise.h
+++ include/cglm/noise.h
@@ -3,6 +3,14 @@
  *
  * MIT License (MIT), http://opensource.org/licenses/MIT
  * Full license can be found in the LICENSE file
+ *
+ * Based on the work of Stefan Gustavson and Ashima Arts on "webgl-noise":
+ * https://github.com/stegu/webgl-noise
+ * Following Stefan Gustavson's paper "Simplex noise demystified":
+ * http://www.itn.liu.se/~stegu/simplexnoise/simplexnoise.pdf
+ * 
+ * Implementation based on glm::glc::noise.hpp:
+ * https://github.com/g-truc/glm/blob/master/glm/gtc/noise.inl
  */
 
 #ifndef cglm_noise_h
