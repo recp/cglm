f0f05dadc57d02d0d48a34a972d667054a8e2a62
Davide Del Zompo <davide.delzompo@gmail.com>
2026-03-22 11:11:46 +0200

    Move glm_euler_seq to types.h

--- include/cglm/euler.h
+++ include/cglm/euler.h
@@ -83,25 +83,6 @@
 #  include "clipspace/view_rh_no.h"
 #endif
 
-
-/*!
- * if you have axis order like vec3 orderVec = [0, 1, 2] or [0, 2, 1]...
- * vector then you can convert it to this enum by doing this:
- * @code
- * glm_euler_seq order;
- * order = orderVec[0] | orderVec[1] << 2 | orderVec[2] << 4;
- * @endcode
- * you may need to explicit cast if required
- */
-typedef enum glm_euler_seq {
-  GLM_EULER_XYZ = 0 << 0 | 1 << 2 | 2 << 4,
-  GLM_EULER_XZY = 0 << 0 | 2 << 2 | 1 << 4,
-  GLM_EULER_YZX = 1 << 0 | 2 << 2 | 0 << 4,
-  GLM_EULER_YXZ = 1 << 0 | 0 << 2 | 2 << 4,
-  GLM_EULER_ZXY = 2 << 0 | 0 << 2 | 1 << 4,
-  GLM_EULER_ZYX = 2 << 0 | 1 << 2 | 0 << 4
-} glm_euler_seq;
-
 CGLM_INLINE
 glm_euler_seq
 glm_euler_order(int ord[3]) {
